bed9a9de1afaa2d795ba328d35b2587b05ed9992
y16ra <y16ra@users.noreply.github.com>
2022-04-16 00:14:00 +0900

    add gRPC app

---
+++ cmd/server/server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/y16ra/hello-grpc/hello"
+	"google.golang.org/grpc"
+)
+
+var (
+	port = flag.Int("port", 50051, "The server port")
+)
+
+type server struct {
+	hello.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *hello.HelloRequest) (*hello.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &hello.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
+
+func main() {
+	flag.Parse()
+	// Specify the port number
+	listenPort, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	hello.RegisterGreeterServer(s, &server{})
+	log.Printf("server listening at %v", listenPort.Addr())
+	if err := s.Serve(listenPort); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
